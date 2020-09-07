09c28db81260d1922a72f0d631244de8d7e9b250
zenzen <jasen0324@naver.com>
2020-09-07 17:40:20 +0900

    섹션2 7강완료

---
+++ unityphoton/Assets/Script/LaunchManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+namespace Script
+{
+    public class LaunchManager : MonoBehaviourPunCallbacks
+    {
+        public void ConnectToPhotonServer()
+        {
+            if (!PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log($"{PhotonNetwork.NickName} 이름으로 포톤서버에 연결되었습니다.");
+        }
+
+        public override void OnConnected()
+        {
+            Debug.Log("인터넷에 연결되었습니다.");
+        }
+    }
+}
